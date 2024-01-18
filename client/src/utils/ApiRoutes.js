91fafe75dd3d20ed544040f0c9918fecb7d09cdc
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-01-19 03:01:48 +0700

    updated apiroutes

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,3 +1,4 @@
 export const host="https://api-blogs-app.vercel.app";
 export const signinRoutes= `${host}/api/auth/signin`;
 export const signupRoutes= `${host}/api/auth/signup`;
+export const signoutRoutes= `${host}/api/auth/signout`;
