10ad209fa6cf48bd01240547d04ed6b7b74a5a1c
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-01-19 02:41:33 +0700

    second commits utils

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,3 +1,3 @@
-export const host="https://mern-blogs-api.vercel.app";
+export const host="https://api-blogs-app.vercel.app";
 export const signinRoutes= `${host}/api/auth/signin`;
 export const signupRoutes= `${host}/api/auth/signup`;
